88cc8a6f4836e7773b9b82058bdd39bc67608c72
fadhly-git <fakeherex160@gmail.com>
2025-06-23 21:57:14 +0700

    update models

--- app/Models/Absensi.php
+++ app/Models/Absensi.php
@@ -2,21 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Absensi extends Model
 {
-    use HasFactory;
     protected $table = 'absensis';
-    protected $fillable = [
-        'id_siswa',
-        'tanggal',
-        'notes',
-        'bonus'
+    protected $fillable = ['id_siswa', 'tanggal', 'notes', 'bonus'];
+
+    protected $casts = [
+        'bonus' => 'boolean',
+        'tanggal' => 'date',
     ];
 
-    public function siswa()
+    public function siswa(): BelongsTo
     {
         return $this->belongsTo(Siswa::class, 'id_siswa');
     }
